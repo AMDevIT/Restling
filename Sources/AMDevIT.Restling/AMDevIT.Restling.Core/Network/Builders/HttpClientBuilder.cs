f79d406a48a3515c18fc734fc7c576df46ef6cb3
Alessandro Morvillo <a.morvillo@amdev.it>
2025-01-28 13:39:10 +0100

    Added HttpClientBuilder
    Added HttpClient builder class for initialize HttpClient with a new message handler and cookies.

---
+++ Sources/AMDevIT.Restling/AMDevIT.Restling.Core/Network/Builders/HttpClientBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+
+namespace AMDevIT.Restling.Core.Network.Builders
+{
+    public class HttpClientBuilder
+    {
+        #region Fields
+
+        private HttpMessageHandler? httpMessageHandler;
+        private CookieContainer? cookieContainer;
+        private bool disposeHandler = false;
+
+        #endregion
+
+        #region Methods
+
+        public HttpClientBuilder AddCookieContainer(CookieContainer cookieContainer)
+        {
+            this.cookieContainer = cookieContainer;
+            return this;
+        }
+
+        public HttpClientBuilder AddHandler(HttpMessageHandler handler, bool diposeHandler = false)
+        {
+            this.httpMessageHandler = handler;
+            this.disposeHandler = diposeHandler;
+
+            return this;
+        }
+
+        public HttpClient Build()
+        {
+            HttpClient httpClient;
+
+            if (this.httpMessageHandler != null)
+                httpClient = new(this.httpMessageHandler, this.disposeHandler);
+            else
+                httpClient = new()
+
+            return httpClient;
+        }
+
+
+        #endregion
+    }
+}
